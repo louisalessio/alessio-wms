0d1e5ecf318daab92d13ca2bd1b0e165649c01c0
Louis Alessio <louisalessio.13@gmail.com>
2026-06-17 19:59:03 +0200

    first commit of WMS project

---
+++ src/main/java/com/alessio/wms/AlessioWmsApplication.java
@@ -0,0 +1,13 @@
+package com.alessio.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlessioWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlessioWmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alessio/wms/AlessioWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alessio.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlessioWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
