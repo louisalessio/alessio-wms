9b7071b14629b20848884bcbc75b1b48a9cf41b7
Louis Alessio <louisalessio.13@gmail.com>
2026-06-21 18:20:13 +0200

    setup testing infrastructure with testcontainers and flyway

---
+++ src/test/java/com/alessio/wms/BaseIntegrationTest.java
@@ -0,0 +1,15 @@
+package com.alessio.wms;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+public abstract class BaseIntegrationTest {
+    @Container
+    @ServiceConnection
+    protected static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:18-alpine");
+}
